94024e0ad144616910a42e2861bda3f1d18acd78
Hack Reactor Students <accounts@hackreactor.com>
2015-12-30 15:35:56 -0800

    Port number uses environment variable, updated sqlite3 dependency to 3.1.1

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 var app = require('./server-config.js');
 
-var port = 4568;
-
+//var port = 4568;
+// ENV
+var port = process.env.PORT;
 app.listen(port);
 
 console.log('Server now listening on port ' + port);
