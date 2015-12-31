61611d35886af9206d68aa83c217835665aafe61
Hack Reactor Students <accounts@hackreactor.com>
2015-12-30 22:01:59 -0800

    refactor models and config to mongoose

--- app/models/link.js
+++ app/models/link.js
@@ -1,19 +1,20 @@
 var db = require('../config');
 var crypto = require('crypto');
+var mongoose = require('mongoose');
+var linkSchema = mongoose.Schema({
+  url:String,
+  base_url:String,
+  code: String,
+  title:String,
+  visits: Number
+});
+var Link = mongoose.model('link', linkSchema);
 
-var Link = db.Model.extend({
-  tableName: 'urls',
-  hasTimestamps: true,
-  defaults: {
-    visits: 0
-  },
-  initialize: function(){
-    this.on('creating', function(model, attrs, options){
+linkSchema.pre('save', function(){
       var shasum = crypto.createHash('sha1');
-      shasum.update(model.get('url'));
-      model.set('code', shasum.digest('hex').slice(0, 5));
-    });
-  }
+      shasum.update(this.url);
+      this.code = shasum.digest('hex').slice(0, 5));
+      next();
 });
 
 module.exports = Link;
